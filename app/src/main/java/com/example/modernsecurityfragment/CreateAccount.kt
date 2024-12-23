d7160960e13ee5915bb95f57258245c5e38a9f45
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-12-23 13:15:52 +0200

    kotlin

--- app/src/main/java/com/example/modernsecurityfragment/CreateAccount.kt
+++ app/src/main/java/com/example/modernsecurityfragment/CreateAccount.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 
 class CreateAccount : Fragment() {
 
@@ -14,6 +16,12 @@ class CreateAccount : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_create_account, container, false)
+
+        val redirectLink = view.findViewById<TextView>(R.id.login_redirect_link)
+
+        redirectLink.setOnClickListener { v ->
+            findNavController().navigate(R.id.login)
+        }
 
         return view
     }
--- app/src/main/java/com/example/modernsecurityfragment/Login.kt
+++ app/src/main/java/com/example/modernsecurityfragment/Login.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 
 class Login : Fragment() {
 
@@ -14,6 +16,12 @@ class Login : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_login, container, false)
+
+        val redirectLink = view.findViewById<TextView>(R.id.create_account_redirect)
+
+        redirectLink.setOnClickListener { view ->
+            findNavController().navigate(R.id.createAccount)
+        }
 
         return view
     }
